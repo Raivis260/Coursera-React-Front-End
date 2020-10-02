ab7ec10b36b01abb1dfad65bbf078a2e30a8833c
Raivydas Česlauskas <raivydas.ceslauskas@gmail.com>
2020-10-02 18:17:52 +0300

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
-import  { addComment } from '../redux/ActionCreators';
+import  { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -20,7 +20,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {dispatch(fetchDishes())}
 })
 
 class Main extends Component {
@@ -29,11 +30,17 @@ class Main extends Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
 
     const HomePage = () => {
       return(
-        <Home dish ={this.props.dishes.filter((dish) => dish.featured === true)[0]}
+        <Home dish ={this.props.dishes.dishes.filter((dish) => dish.featured === true)[0]}
+        dishesLoading={this.props.dishes.isLoading}
+        dishesErrMessage={this.props.dishes.errMessage}
         promotion = {this.props.promotions.filter((promotion) => promotion.featured === true)[0]}
         leader = {this.props.leaders.filter((leader) => leader.featured === true)[0]}
         />
@@ -42,7 +49,9 @@ class Main extends Component {
 
     const DishWithId = ( {match} ) => {
       return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        isLoading={this.props.dishes.isLoading}
+        errMessage={this.props.dishes.errMessage}
         comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
         addComment={this.props.addComment}
         />
@@ -60,7 +69,7 @@ class Main extends Component {
       <Header />
       <Switch>
         <Route path="/home" component={HomePage} />
-        <Route exact path="/menu" component={() => < Menu dishes={this.props.dishes}/> } />
+        <Route exact path="/menu" component={() => < Menu dishes={this.props.dishes.dishes}/> } />
         <Route path = "/menu/:dishId" component ={DishWithId} />
         <Route exact path="/contactus" component={Contact} />
         <Route exact path="/aboutus" component={AboutPage} />
